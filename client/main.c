66fe10dbca5f71161bd09191a7068db15a56e117
denisSurkov <surkov0denis@yandex.ru>
2023-05-20 17:13:05 +0500

    added client example working with reading stdin

--- client/main.c
+++ client/main.c
@@ -4,34 +4,72 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <time.h>
+#include <errno.h>
 
 #define SOCKET_NAME "/tmp/test.socket"
 
+int msleep(long msec)
+{
+    struct timespec ts;
+    int res;
 
-int main() {
-    int serverSocket = socket(AF_UNIX, SOCK_STREAM, 0);
-
-    struct sockaddr_un serverSocketAddr;
-    memset(&serverSocketAddr, 1, sizeof(struct sockaddr_un));
-
-    serverSocketAddr.sun_family = AF_UNIX;
-    strcpy(serverSocketAddr.sun_path, SOCKET_NAME);
-
-    int ok = connect(serverSocket, (struct sockaddr *) &serverSocketAddr, sizeof(serverSocketAddr));
-    if (ok < 0) {
-        perror("connect");
-        exit(-1);
+    if (msec < 0)
+    {
+        errno = EINVAL;
+        return -1;
     }
 
-    char buffer[10];
-    strcpy(buffer, "0\n");
-    send(serverSocket, buffer, sizeof(buffer), 0);
-    perror("send");
-    printf("%s", buffer);
+    ts.tv_sec = msec / 1000;
+    ts.tv_nsec = (msec % 1000) * 1000000;
 
-    recv(serverSocket, buffer, 10, 0);
-    printf("%s", buffer);
+    do {
+        res = nanosleep(&ts, &ts);
+    } while (res && errno == EINTR);
 
-    printf("Hello, World!\n");
+    return res;
+}
+
+int main() {
+//    int serverSocket = socket(AF_UNIX, SOCK_STREAM, 0);
+//
+//    struct sockaddr_un serverSocketAddr;
+//    memset(&serverSocketAddr, 1, sizeof(struct sockaddr_un));
+//
+//    serverSocketAddr.sun_family = AF_UNIX;
+//    strcpy(serverSocketAddr.sun_path, SOCKET_NAME);
+//
+//    int ok = connect(serverSocket, (struct sockaddr *) &serverSocketAddr, sizeof(serverSocketAddr));
+//    if (ok < 0) {
+//        perror("connect");
+//        exit(-1);
+//    }
+    srand(time(NULL));
+
+    unsigned short r = rand() % 200;
+    printf("%d", r);
+    unsigned int byteRead = 0;
+    char stdinBuffer;
+    while (fread(&stdinBuffer, 1, 1, stdin) > 0) {
+        byteRead++;
+        if (byteRead == r) {
+            msleep(300);
+        }
+        printf("%c", stdinBuffer);
+    }
+
+    printf("%d %d", r, byteRead);
+
+//
+//    char buffer[10];
+//    strcpy(buffer, "0\n");
+//    send(serverSocket, buffer, sizeof(buffer), 0);
+//    perror("send");
+//    printf("%s", buffer);
+//
+//    recv(serverSocket, buffer, 10, 0);
+//    printf("%s", buffer);
+//
+//    printf("Hello, World!\n");
     return 0;
 }
